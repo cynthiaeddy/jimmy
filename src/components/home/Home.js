eefca147e12774140603e0a5862a0a77ff021f03
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2020-11-15 18:11:44 -0500

    starting design

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import mexico from '../../assets/images/mexico.jpg'
-import paris from '../../assets/images/paris.jpg'
+import opener from '../../assets/images/jim indesign3 1.png'
 
 
 const Home = () => {
   return (
     <div className='home'>
 
-      <h1>James Oseland's World Food Books</h1>
-      <img src={mexico} alt='Mexico Book Cover'></img>
-      <img src={paris} alt='Paris Book Cover'></img>
+      <h1>World Food Books</h1>
+      <img src={opener} className='opener'alt='opener'></img>
 
 
     </div>

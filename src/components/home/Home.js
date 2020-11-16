fd4bf4f0a8fd0446db67fbebc93f0d0dac743431
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2020-11-16 14:45:08 -0500

    home page css

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import '../../stylesheets/home.css'
 import mexico from '../../assets/images/mexico.jpg'
+import coverSm from '../../assets/images/coverSm.jpg'
+
 import opener from '../../assets/images/jim indesign3 1.png'
+
 import openerBlue from '../../assets/images/openerBlue.jpg'
 import openerCorn from '../../assets/images/openerCorn.jpg'
 import openerDessert from '../../assets/images/openerDessert.jpg'
 import openerFighter from '../../assets/images/openerFighter.jpg'
 import openerFlan from '../../assets/images/openerFlan.jpg'
 import openergal from '../../assets/images/openergal.jpg'
+import openerWhite from '../../assets/images/openerWhite.jpg'
+
 import openerMan from '../../assets/images/openerMan.jpg'
 import openerOysters from '../../assets/images/openerOysters.jpg'
 import openerRed from '../../assets/images/openerRed.jpg'
@@ -38,10 +44,14 @@ const Home = () => {
         <div className='interior right'>
           <img src={openerBlue} className='righty'></img>
           <img src={openerWaiter} className='righty'></img>
-          <img src={openerOysters} className='righty white'></img>
+          <img src={openerWhite} className='righty white'>
+          </img>
+          {/* <div className='righty whitebox'></div> */}
           <img src={openerOysters} className='righty'></img>
           <img src={openerRed} className='righty'></img>
           <img src={openerFighter} className='righty'></img>
+          <div className='homeDek'><h4>dek goes here something this long maybe to fill this space another line</h4></div>
+
         </div>
 
       </section>

ee6626916b8ec1fdf3c924a7354139a06a834261
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2020-11-17 22:14:37 -0500

    net

--- src/components/aboutWF/AboutWF.js
+++ src/components/aboutWF/AboutWF.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import imageData from '../carouselMexico/Images'
+
 // import Images from '../carouselMexico/Images'
-import ForwardArrow from '../carouselMexico/ForwardArrow'
-import BackArrow from '../carouselMexico/BackArrow'
+// import ForwardArrow from '../carouselMexico/ForwardArrow'
+// import BackArrow from '../carouselMexico/BackArrow'
 // import Slide from '../carouselMexico/Slide'
 
 
@@ -9,20 +11,29 @@ import '../../stylesheets/AboutWF.css'
 
 import cactusSpread from '../../assets/images/cactusSpread.jpg'
 
-const AboutWF = () => {
-  return (
-    // <div className='wf-container'>
-    <div className='wf-content'>
-     <h2><div className='back'>back</div></h2>
+class AboutWF extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeIndex: 0,
+      length: imageData.length
+    };
+  }
+  render() {
+    return (
+      // <div className='wf-container'>
+      <div className='wf-content'>
+        <h2><div className='back'>back</div></h2>
 
-<div className='main'>
-      <a href target="#"> <h2>buy me text will be in navbar</h2></a><img src={cactusSpread} className='spread' alt='spread'></img>
-      </div>
-      <h2><div className='forward'>forward</div></h2>
+        <div className='main'>
+          <a href target="#"> <h2>buy me text will be in navbar</h2></a><img src={cactusSpread} className='spread' alt='spread'></img>
+        </div>
+        <h2><div className='forward'>forward</div></h2>
       </div>
       // </div>
 
-  );
-};
+    );
+  };
+}
 
 export default AboutWF;

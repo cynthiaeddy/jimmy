d9f83b2bf6a75d335c2a2ac1fc7cf322b3b8d81e
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2020-11-17 21:39:28 -0500

    netlify

---
+++ src/components/stash/AboutWF.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import mexico from '../../assets/images/mexico.jpg'
+import '../../stylesheets/AboutWF.css'
+
+import cactusSpread from '../../assets/images/cactusSpread.jpg'
+
+const AboutWF = () => {
+  return (
+    <div className='wf-container'>
+    <div className='wf-content'>
+     <h2><div className='back'>back</div></h2>
+
+<div className='main'>
+      <a href target="#"> <h2>buy me text will be in navbar</h2></a><img src={cactusSpread} className='spread' alt='spread'></img>
+      </div>
+      <h2><div className='forward'>forward</div></h2>
+      </div>
+      </div>
+
+  );
+};
+
+export default AboutWF;
